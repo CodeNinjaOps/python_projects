de8e571e3056e68eb84435d4e2ba2c4ccdb7877f
Your Name <you@example.com>
2024-09-08 07:31:20 +0530

    len_check.py updated the print function

--- P1/len_check.py
+++ P1/len_check.py
@@ -1,11 +1,11 @@
 text = input("Enter a title: ")
 
 len_check = len(text)
-print(len_check)
+print("The Length of the title is ", len_check)
 ===================================================================
 
 print(len(input("Enter a title: ")))
 ====================================================================
 
 text1 = len(input("Enter a title: "))
-print(text1)
+print("The Length of the title is ", text1)
