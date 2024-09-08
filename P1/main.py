9da606850310af73c0410a3c42fbeca0d11eaba7
Your Name <you@example.com>
2024-09-08 11:58:51 +0530

    for loop used in show

--- P1/main.py
+++ P1/main.py
@@ -8,6 +8,7 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
         case 'exit':
             break
