64933d23bb6f0b14cf23dd70be7c6d1c462e9c03
Your Name <you@example.com>
2024-09-08 07:25:25 +0530

    bonus len check

---
+++ P1/len_check.py
@@ -0,0 +1,11 @@
+text = input("Enter a title: ")
+
+len_check = len(text)
+print(len_check)
+===================================================================
+
+print(len(input("Enter a title: ")))
+====================================================================
+
+text1 = len(input("Enter a title: "))
+print(text1)
