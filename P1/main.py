9d6f2e0bc15077d606d1f564a84b0f49326b7a82
Your Name <you@example.com>
2024-09-09 10:46:31 +0530

    added enumerate in show to get counts

--- P1/main.py
+++ P1/main.py
@@ -8,14 +8,18 @@ while True:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
+            
         case 'show' | 'display':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(index + 1,'-', item)
+                
         case 'edit':
             number = int(input("Number of todo to edit: "))
             new_todo = input("Enter new todo: ")
             todos[number - 1] = new_todo
+            
         case 'exit':
             break
+        
         case whatever:
             print("The Command You Enter Is Unknown")
