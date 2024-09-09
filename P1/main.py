a265672636467c120f2efb4d258ef6527e435109
Your Name <you@example.com>
2024-09-09 08:25:13 +0530

    added whatever and display in show

--- P1/main.py
+++ P1/main.py
@@ -2,13 +2,16 @@ todos = []
 
 while True:
     user_action = input("Type add,show or exit: ")
+    user_action = user_action.strip()   
     
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
+        case 'show' | 'display':
             for item in todos:
                 print(item)
         case 'exit':
             break
+        case whatever:
+            print("The Command You Enter Is Unknown")
