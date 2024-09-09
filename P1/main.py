49059fba7515c26f36fb312b4aa1af023c02b66b
Your Name <you@example.com>
2024-09-09 13:02:44 +0530

    complete todo has been added

--- P1/main.py
+++ P1/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add,show,edit or exit: ")
+    user_action = input("Type add,show,edit,completed or exit: ")
     user_action = user_action.strip()   
     
     match user_action:
@@ -11,12 +11,17 @@ while True:
             
         case 'show' | 'display':
             for index, item in enumerate(todos):
-                print(index + 1,'-', item)
+                row = f"{index + 1}-{item}"
+                print(row)
                 
         case 'edit':
             number = int(input("Number of todo to edit: "))
             new_todo = input("Enter new todo: ")
             todos[number - 1] = new_todo
+            
+        case 'complete':
+            number = int(input("Number of todo to complete: "))
+            todos.pop(number - 1)
             
         case 'exit':
             break
