e268c796cad51fa68720f675a4f0f7579b9b9890
Your Name <you@example.com>
2024-09-11 18:04:50 +0530

    function and multiple arguments

--- P1/main.py
+++ P1/main.py
@@ -1,12 +1,11 @@
-def get_todos():
-    with open('todo.txt', 'r') as file:
-        todos = file.readlines()
-    return todos
+def get_todos(filename):
+    with open(filename, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
 
-def modify_todos():
-    with open('todo.txt','w') as file:
-        file.writelines(todos)
-    return todos
+def modify_todos(filename, todos_arg):
+    with open(filename,'w') as file_local:
+        file_local.writelines(todos_arg)
     
 
 while True:
@@ -16,15 +15,15 @@ while True:
     if user_action.startswith("add"):
         todo = (user_action[4:] + "\n")
         
-        todos = get_todos()
+        todos = get_todos("todo.txt")
                 
         todos.append(todo)
         
-        todos = modify_todos()
+        todos = modify_todos("todo.txt", todos)
                         
     elif user_action.startswith("show"):
         
-        todos = get_todos()        
+        todos = get_todos("todo.txt")        
         new_todos = [item.strip('\n') for item in todos]
         
         for index, item in enumerate(new_todos):
@@ -35,11 +34,12 @@ while True:
         try:
             number = int(user_action[5:])
 
-            todos = get_todos()    
+            todos = get_todos("todo.txt")
                 
-            todos = get_todos()
+            new_todo = input("Enter new todo: ") + '\n'
+            todos[number - 1] = new_todo
             
-            todos = modify_todos()
+            todos = modify_todos("todo.txt", todos)
 
         except ValueError:
             print("your command is invalid")
@@ -49,12 +49,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()    
+            todos = get_todos("todo.txt")    
             
             todo_to_remove = todos[number - 1].strip('\n')
-            todos.pop(todo_to_remove - 1)
+            todos.pop(number - 1)
 
-            todos = modify_todos()
+            todos = modify_todos("todo.txt", todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
